d8c84e1cc2ee73e3b77c233db1700bd145058a2b
I Ming <chiangming07@gmail.com>
2023-02-05 23:53:33 +0800

    remove

--- Week-4/Assignment-3/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE assignment;
-USE assignment;
-
-CREATE TABLE user(
-    id integer PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL
-);
